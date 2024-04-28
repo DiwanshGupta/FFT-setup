a7ddb05a6235819b36f38f8f92c674142bf15d1e
Ritik <ritik11181@gmail.com>
2024-04-28 14:09:45 +0530

    ...

--- frontend/app/styles/style.ts
+++
@@ -1,7 +0,0 @@
-
-export const styles = {
-    title:"text-[25px] text-black dark:text-white font-[500] text-center py-2",
-    label:"text-[16px] font-Poppins text-black dark:text-white",
-    input:"w-full dark:text-white bg-transparent border h-[40px] px-2 outline-none mt-[10px] font-Poppins",
-    button:"flex flex-row justify-center py-3 px-6 rounded-full cursor-pointer bg-[#2190ff] nin-h-[45px] w-full text-[16px] font-Poppins font-semibold"
-}
